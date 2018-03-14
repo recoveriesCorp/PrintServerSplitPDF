fab85508f3f3eca502dfb29c450d508124958dac
recoveriesCorp <37356785+recoveriesCorp@users.noreply.github.com>
2018-03-14 14:45:53 +1100

    Add files via upload

--- Helper.cs
+++ Helper.cs
@@ -1,4 +1,20 @@
-﻿using iTextSharp.text;
+﻿//   PrintServerSplitPDF is a command line utility to split multi-document pdf files into single document pdf files.
+//   Copyright(C) 2018 recoveriesCorp
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Affero General Public License as
+//   published by the Free Software Foundation, either version 3 of the
+//   License, or(at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with this program.If not, see<http://www.gnu.org/licenses/>.
+
+using iTextSharp.text;
 using iTextSharp.text.pdf;
 using iTextSharp.text.pdf.parser;
 using NLog;
@@ -512,18 +528,3 @@ namespace PrintServerSplitPDF
         }
     }
 }
-//   PrintServerSplitPDF is a command line utility to split multi-document pdf files into single document pdf files.
-//   Copyright(C) 2018 recoveriesCorp
-//
-//   This program is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU Affero General Public License as
-//   published by the Free Software Foundation, either version 3 of the
-//   License, or(at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//   GNU Affero General Public License for more details.
-//
-//   You should have received a copy of the GNU Affero General Public License
-//   along with this program.If not, see<http://www.gnu.org/licenses/>.
--- Program.cs
+++ Program.cs
@@ -1,4 +1,20 @@
-﻿using NLog;
+﻿//   PrintServerSplitPDF is a command line utility to split multi-document pdf files into single document pdf files.
+//   Copyright(C) 2018 recoveriesCorp
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU Affero General Public License as
+//   published by the Free Software Foundation, either version 3 of the
+//   License, or(at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//   GNU Affero General Public License for more details.
+//
+//   You should have received a copy of the GNU Affero General Public License
+//   along with this program.If not, see<http://www.gnu.org/licenses/>.
+
+using NLog;
 using NLog.Targets;
 using System;
 using System.Globalization;
@@ -60,18 +76,3 @@ namespace PrintServerSplitPDF
         }
     }
 }
-//   PrintServerSplitPDF is a command line utility to split multi-document pdf files into single document pdf files.
-//   Copyright(C) 2018 recoveriesCorp
-//
-//   This program is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU Affero General Public License as
-//   published by the Free Software Foundation, either version 3 of the
-//   License, or(at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-//   GNU Affero General Public License for more details.
-//
-//   You should have received a copy of the GNU Affero General Public License
-//   along with this program.If not, see<http://www.gnu.org/licenses/>.
